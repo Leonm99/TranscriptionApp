92bc772acf418228f725d204597fb00c72e716b5
Leon Mischlich <Leon.mischlich@gmx.net>
2024-12-24 13:11:14 +0100

    ✨ feat(UI): Add Settings Screen and settings button to the Transcription Screen.

---
+++ app/src/main/java/com/example/transcriptionapp/ui/screens/SettingsScreen.kt
@@ -0,0 +1,12 @@
+package com.example.transcriptionapp.ui.screens
+
+import androidx.activity.ComponentActivity
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.example.transcriptionapp.ui.components.BottomSheet
+import com.example.transcriptionapp.viewmodel.TranscriptionViewModel
+
+@Composable
+fun SettingsScreen(viewModel: TranscriptionViewModel, activity: ComponentActivity) {
+    Text("Settings Screen")
+}
--- app/src/main/java/com/example/transcriptionapp/ui/screens/TranscriptionScreen.kt
+++ app/src/main/java/com/example/transcriptionapp/ui/screens/TranscriptionScreen.kt
@@ -6,8 +6,16 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,6 +23,7 @@ import androidx.compose.ui.platform.LocalContext
 import com.example.transcriptionapp.ui.components.BottomSheet
 import com.example.transcriptionapp.viewmodel.TranscriptionViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TranscriptionScreen(viewModel: TranscriptionViewModel) {
   val activity = LocalContext.current
@@ -25,6 +34,22 @@ fun TranscriptionScreen(viewModel: TranscriptionViewModel) {
               result.data?.data?.let { uri -> viewModel.onAudioSelected(uri, activity) }
             }
           }
+
+  TopAppBar(
+      title = { Text("TranscriptionApp") },
+      colors =
+          TopAppBarDefaults.topAppBarColors(
+              containerColor = MaterialTheme.colorScheme.primaryContainer,
+              titleContentColor = MaterialTheme.colorScheme.primary,
+          ),
+      actions = {
+        IconButton(
+            onClick = {
+              /* click action */
+            }) {
+              Icon(imageVector = Icons.Filled.Settings, contentDescription = "Settings")
+            }
+      })
 
   Column(
       modifier = Modifier.fillMaxSize(),
