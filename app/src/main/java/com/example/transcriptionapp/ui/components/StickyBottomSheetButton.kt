c132a98bd4158753523a2e9c712f5a96622736ce
Leon Mischlich <Leon.mischlich@gmx.net>
2025-01-07 22:09:25 +0100

    ✨ feat(UI): Implement transcription result display in a card and add translate button.

---
+++ app/src/main/java/com/example/transcriptionapp/ui/components/StickyBottomSheetButton.kt
@@ -0,0 +1,34 @@
+package com.example.transcriptionapp.ui.components
+
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun StickyBottomSheetButton(
+    sheetState: SheetState,
+    onClick: () -> Unit,
+    text: String
+) {
+    Button(
+        modifier = Modifier
+            .wrapContentWidth()
+            .shadow(
+                elevation = 4.dp,
+                shape = RoundedCornerShape(25.dp)
+            ),
+        onClick = onClick
+    ) {
+        Text(text = text)
+    }
+}
